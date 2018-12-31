d4844a0154d0861a6a8d4051e9490bdf0e4027c0
Julian Sweatt <20158985+lordvad3r@users.noreply.github.com>
2018-12-30 21:41:38 -0600

    Theme Control

--- theme_manager.js
+++ theme_manager.js
@@ -1,3 +1,18 @@
-function setTheme(newTheme) {
+// Themes: pride, monochrome_dark, monochrome_light
+
+function setTheme(newTheme)
+{
     document.getElementById('theme').href = ("themes/" + newTheme + ".css");
+}
+
+function initialize()
+{
+    let currentMonth = new Date().getMonth();
+
+    /***** Theme Months *****/
+    /* Pride Month (June) */
+    if(currentMonth == 5)
+    {
+        setTheme("pride")
+    }
 }
